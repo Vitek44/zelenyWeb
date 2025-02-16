fb0c43e7f652426ef7866b6ef39ac89be1375470
Viteja <vitejajan@gmail.com>
2025-02-16 14:32:36 +0100

    Add modal for adding new table with form inputs and styling

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -4,6 +4,8 @@ import AdminNavbar from "../../components/admin-navbar/admin-navbar";
 import "./admin.css";
 
 const Admin = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
   const [data, setData] = useState([]);
 
   const loadData = () => {
@@ -32,10 +34,10 @@ const Admin = () => {
       <div className="admin-wrapper">
         <div className="container">
           <div className="admin-content">
-            <a href="" class="add-card">
+            <div class="add-card" onClick={() => setModalOpen(true)}>
               <h3>Nový stůl</h3>
               <i class="fa-solid fa-plus"></i>
-            </a>
+            </div>
             {data.map((item) => (
               <div className="stul-card">
                 <img src={item.URL} alt="" />
@@ -55,6 +57,52 @@ const Admin = () => {
           </div>
         </div>
       </div>
+      {modalOpen ? (
+        <div className="modal-wrapper">
+          <div class="modal">
+            <div class="modal-header">
+              <h3>Nový stůl</h3>
+              <button className="close-modal" onClick={() => setModalOpen(false)} title="Zavřít okno">
+                <i class="fa-solid fa-xmark"></i>
+              </button>
+            </div>
+            <div class="modal-content">
+              <div class="form-group">
+                <input type="text" name="nazev" id="nazev" placeholder="Název stolu" />
+              </div>
+              <div class="form-group">
+                <input type="text" placeholder="Materiál" />
+              </div>
+              <div class="form-group">
+                <input type="number" placeholder="Výška stolu" />
+                <input type="number" placeholder="Šířka stolu" />
+              </div>
+              <div class="form-group">
+                <input type="number" placeholder="Tloušťka desky" />
+                <input type="number" placeholder="Délka desky" />
+              </div>
+              <div class="form-group">
+                <textarea name="" id="" placeholder="Popis - česky"></textarea>
+              </div>
+              <div class="form-group">
+                <textarea name="" id="" placeholder="Popis - anglicky"></textarea>
+              </div>
+              <div class="form-group">
+                <textarea name="" id="" placeholder="Popis - německy"></textarea>
+              </div>
+              <div class="form-group">
+                <input type="text" name="" id="" placeholder="Cena" />
+              </div>
+              <div class="form-group">
+                <input type="file" name="" id="" />
+              </div>
+              <button className="save-btn">Uložit</button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 };
