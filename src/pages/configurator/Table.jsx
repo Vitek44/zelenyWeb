79d12ef8d348cfc55a4e705258a74e23bd80fc29
Viteja <vitejajan@gmail.com>
2025-03-25 20:13:04 +0100

    Add SVG images and update configurator styles for improved layout and usability

--- src/pages/configurator/Table.jsx
+++ src/pages/configurator/Table.jsx
@@ -29,189 +29,44 @@ const Table = (props) => {
   });
   return (
     <group {...props} dispose={null}>
-      <mesh
-        geometry={nodes["6table"].geometry}
-        material={nodes["6table"].material}
-        position={[0, 1.302, 0]}
-        scale={[1.425, 0.043, 1.425]}
-        visible={shape === 6}
-      >
-        <meshStandardMaterial
-          {...(material === "wood1" ? wood1TextureProps : wood2TextureProps)}
-        />
+      <mesh geometry={nodes["6table"].geometry} material={nodes["6table"].material} position={[0, 1.302, 0]} scale={[1.425, 0.043, 1.425]} visible={shape === 6}>
+        <meshStandardMaterial {...(material === "wood1" ? wood1TextureProps : wood2TextureProps)} />
       </mesh>
-      <mesh
-        geometry={nodes["8table"].geometry}
-        material={nodes["8table"].material}
-        position={[0, 1.302, 0]}
-        scale={[1.425, 0.043, 1.425]}
-        visible={shape === 5}
-      >
-        <meshStandardMaterial
-          {...(material === "wood1" ? wood1TextureProps : wood2TextureProps)}
-        />
+      <mesh geometry={nodes["8table"].geometry} material={nodes["8table"].material} position={[0, 1.302, 0]} scale={[1.425, 0.043, 1.425]} visible={shape === 5}>
+        <meshStandardMaterial {...(material === "wood1" ? wood1TextureProps : wood2TextureProps)} />
       </mesh>
 
-      <mesh
-        geometry={nodes.Plane.geometry}
-        material={nodes.Plane.material}
-        position={[0, 1.295, 0]}
-        rotation={[-Math.PI, -1.571, 0]}
-        scale={[1.639, 1, 1]}
-        visible={shape === 4}
-      >
-        <meshStandardMaterial
-          {...(material === "wood1" ? wood1TextureProps : wood2TextureProps)}
-        />
+      <mesh geometry={nodes.Plane.geometry} material={nodes.Plane.material} position={[0, 1.342, 0]} rotation={[-Math.PI, -1.571, 0]} scale={[1.639, 1, 1.2]} visible={shape === 4}>
+        <meshStandardMaterial {...(material === "wood1" ? wood1TextureProps : wood2TextureProps)} />
       </mesh>
 
-      <mesh
-        geometry={nodes.obdelnik.geometry}
-        position={[0, 1.302, 0]}
-        scale={[1.129, 0.043, 1.129]}
-        visible={shape === 1}
-      >
-        <meshStandardMaterial
-          {...(material === "wood1" ? wood1TextureProps : wood2TextureProps)}
-        />
+      <mesh geometry={nodes.obdelnik.geometry} position={[0, 1.302, 0]} scale={[1.129, 0.043, 1.129]} visible={shape === 1}>
+        <meshStandardMaterial {...(material === "wood1" ? wood1TextureProps : wood2TextureProps)} />
       </mesh>
-      <mesh
-        geometry={nodes.cverec1.geometry}
-        material={materials["Material.005"]}
-        position={[0, 1.295, 0]}
-        scale={[1.174, 0.037, 1.174]}
-        visible={shape === 2}
-      >
-        <meshStandardMaterial
-          {...(material === "wood1" ? wood1TextureProps : wood2TextureProps)}
-        />
+      <mesh geometry={nodes.cverec1.geometry} material={materials["Material.005"]} position={[0, 1.295, 0]} scale={[1.174, 0.037, 1.174]} visible={shape === 2}>
+        <meshStandardMaterial {...(material === "wood1" ? wood1TextureProps : wood2TextureProps)} />
       </mesh>
-      <mesh
-        geometry={nodes.kruh.geometry}
-        material={materials.dřevo}
-        position={[0, 1.302, 0]}
-        scale={[1.455, 0.043, 1.455]}
-        visible={shape === 3}
-      >
-        <meshStandardMaterial
-          {...(material === "wood1" ? wood1TextureProps : wood2TextureProps)}
-        />
+      <mesh geometry={nodes.kruh.geometry} material={materials.dřevo} position={[0, 1.302, 0]} scale={[1.455, 0.043, 1.455]} visible={shape === 3}>
+        <meshStandardMaterial {...(material === "wood1" ? wood1TextureProps : wood2TextureProps)} />
       </mesh>
 
-      <mesh
-        geometry={nodes.ctverec003.geometry}
-        material={materials["Material.011"]}
-        position={[0, 1.295, 0]}
-        scale={[0.918, 0.037, 0.918]}
-        visible={shape === 50}
-      />
-      <mesh
-        geometry={nodes.ctverec2.geometry}
-        material={materials["Material.014"]}
-        position={[0, 1.295, 0]}
-        scale={[1.174, 0.037, 1.174]}
-        visible={shape === 51}
-      />
-      <mesh
-        geometry={nodes.Nohy001.geometry}
-        material={materials["Material.010"]}
-        position={[0, 1.295, 0]}
-        scale={[1, 0.037, 1]}
-        visible={legs === 1}
-      />
-      <mesh
-        geometry={nodes.Nohy001.geometry}
-        material={materials["Material.010"]}
-        position={[0, 1.295, 1.7]}
-        scale={[1, 0.037, 1]}
-        visible={legs === 1}
-      />
-      <mesh
-        geometry={nodes.Nohy02.geometry}
-        material={materials["Material.010"]}
-        position={[0, 0.602, 0]}
-        rotation={[0, Math.PI / 2, 0]}
-        scale={[0.208, 0.033, 0.72]}
-        visible={legs === 2}
-      />
-      <mesh
-        geometry={nodes.Nohy02.geometry}
-        material={materials["Material.010"]}
-        position={[0, 0.602, 1.6]}
-        rotation={[0, Math.PI / 2, 0]}
-        scale={[0.208, 0.033, 0.72]}
-        visible={legs === 2}
-      />
-      <mesh
-        geometry={nodes.nohy03.geometry}
-        material={materials["Material.010"]}
-        position={[0, 1.295, 0]}
-        scale={[0.918, 0.037, 0.918]}
-        visible={legs === 3}
-      />
-      <mesh
-        geometry={nodes.nohy03.geometry}
-        material={materials["Material.010"]}
-        position={[0, 1.295, 0, 4]}
-        rotation={[0, Math.PI, 0]}
-        scale={[0.918, 0.037, 0.918]}
-        visible={legs === 3}
-      />
-      <mesh
-        geometry={nodes.nohy04.geometry}
-        material={materials["Material.010"]}
-        position={[0, 1.312, 0]}
-        scale={[0.918, 0.049, 0.918]}
-        visible={legs === 4}
-      />
-      <mesh
-        geometry={nodes.nohy04.geometry}
-        material={materials["Material.010"]}
-        position={[0, 1.312, 1.2]}
-        scale={[0.918, 0.049, 0.918]}
-        visible={legs === 4}
-      />
-      <mesh
-        geometry={nodes.nohy05.geometry}
-        material={materials["Material.010"]}
-        position={[0, 1.295, 0]}
-        scale={[0.918, 0.037, 0.918]}
-        visible={legs === 5}
-      />
+      <mesh geometry={nodes.ctverec003.geometry} material={materials["Material.011"]} position={[0, 1.295, 0]} scale={[0.918, 0.037, 0.918]} visible={shape === 50} />
+      <mesh geometry={nodes.ctverec2.geometry} material={materials["Material.014"]} position={[0, 1.295, 0]} scale={[1.174, 0.037, 1.174]} visible={shape === 51} />
+      <mesh geometry={nodes.Nohy001.geometry} material={materials["Material.010"]} position={[0, 1.295, 0]} scale={[1, 0.037, 1]} visible={legs === 1} />
+      <mesh geometry={nodes.Nohy001.geometry} material={materials["Material.010"]} position={[0, 1.295, 1.7]} scale={[1, 0.037, 1]} visible={legs === 1} />
+      <mesh geometry={nodes.Nohy02.geometry} material={materials["Material.010"]} position={[0, 0.602, 0]} rotation={[0, Math.PI / 2, 0]} scale={[0.208, 0.033, 0.72]} visible={legs === 2} />
+      <mesh geometry={nodes.Nohy02.geometry} material={materials["Material.010"]} position={[0, 0.602, 1.6]} rotation={[0, Math.PI / 2, 0]} scale={[0.208, 0.033, 0.72]} visible={legs === 2} />
+      <mesh geometry={nodes.nohy03.geometry} material={materials["Material.010"]} position={[0, 1.295, 0]} scale={[0.918, 0.037, 0.918]} visible={legs === 3} />
+      <mesh geometry={nodes.nohy03.geometry} material={materials["Material.010"]} position={[0, 1.295, 0, 4]} rotation={[0, Math.PI, 0]} scale={[0.918, 0.037, 0.918]} visible={legs === 3} />
+      <mesh geometry={nodes.nohy04.geometry} material={materials["Material.010"]} position={[0, 1.312, 0]} scale={[0.918, 0.049, 0.918]} visible={legs === 4} />
+      <mesh geometry={nodes.nohy04.geometry} material={materials["Material.010"]} position={[0, 1.312, 1.2]} scale={[0.918, 0.049, 0.918]} visible={legs === 4} />
+      <mesh geometry={nodes.nohy05.geometry} material={materials["Material.010"]} position={[0, 1.295, 0]} scale={[0.918, 0.037, 0.918]} visible={legs === 5} />
 
-      <mesh
-        geometry={nodes.nohy05.geometry}
-        material={materials["Material.010"]}
-        position={[0, 1.295, 0.08]}
-        rotation={[0, Math.PI, 0]}
-        scale={[0.918, 0.037, 0.918]}
-        visible={legs === 5}
-      />
-      <mesh
-        geometry={nodes.nohy06.geometry}
-        material={materials["Material.010"]}
-        position={[0, 0.602, 0]}
-        rotation={[0, Math.PI / 2, 0]}
-        scale={[0.208, 0.033, 0.72]}
-        visible={legs === 6}
-      />
+      <mesh geometry={nodes.nohy05.geometry} material={materials["Material.010"]} position={[0, 1.295, 0.08]} rotation={[0, Math.PI, 0]} scale={[0.918, 0.037, 0.918]} visible={legs === 5} />
+      <mesh geometry={nodes.nohy06.geometry} material={materials["Material.010"]} position={[0, 0.602, 0]} rotation={[0, Math.PI / 2, 0]} scale={[0.208, 0.033, 0.72]} visible={legs === 6} />
 
-      <mesh
-        geometry={nodes.nohy07.geometry}
-        material={materials["Material.010"]}
-        position={[0, 1.295, 0]}
-        rotation={[0, Math.PI / 2, 0]}
-        scale={[1.011, 0.032, 1.011]}
-        visible={legs === 7}
-      />
-      <mesh
-        geometry={nodes.nohy07.geometry}
-        material={materials["Material.010"]}
-        position={[0, 1.295, 1]}
-        rotation={[0, Math.PI / 2, 0]}
-        scale={[1.011, 0.032, 1.011]}
-        visible={legs === 7}
-      />
+      <mesh geometry={nodes.nohy07.geometry} material={materials["Material.010"]} position={[0, 1.295, 0]} rotation={[0, Math.PI / 2, 0]} scale={[1.011, 0.032, 1.011]} visible={legs === 7} />
+      <mesh geometry={nodes.nohy07.geometry} material={materials["Material.010"]} position={[0, 1.295, 1]} rotation={[0, Math.PI / 2, 0]} scale={[1.011, 0.032, 1.011]} visible={legs === 7} />
     </group>
   );
 };
